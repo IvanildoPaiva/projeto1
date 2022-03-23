2c5c9b55fd1887d7f6fc841cd70cb2343e075da3
Ivanildopaiva <ivanildo_ap@hotmail.com>
2022-03-23 16:19:48 -0400

    Preenchimento do resultado da busca do CEP

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-function consutaEndereco(){
-	
+'use strict';
+
+const preencherForm = (retorno)  => {
+document.getElementById('endereco').value = retorno.logradouro;
+document.getElementById('bairro').value = retorno.bairro;
+document.getElementById('cidade').value = retorno.localidade;
+document.getElementById('estado').value = retorno.uf;
 }
+
+const pesquisaCep = async() =>{
+    const cep = document.getElementById('cep').value;
+
+    const url =  `https://viacep.com.br/ws/${cep}/json/`;
+    // fetch(url).then(response => response.json()).then(console.log);
+
+   const busca = await fetch(url);
+   const retorno = await busca.json();
+    preencherForm(retorno);
+}
+
+
+document.getElementById('cep').addEventListener('focusout', pesquisaCep);
