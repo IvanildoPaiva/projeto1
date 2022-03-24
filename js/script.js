f278e368cca545a02e9668cc3f72c26857b597f2
Ivanildopaiva <ivanildo_ap@hotmail.com>
2022-03-24 08:16:19 -0400

    validação do campo CEP

--- js/script.js
+++ js/script.js
@@ -7,16 +7,34 @@ document.getElementById('cidade').value = retorno.localidade;
 document.getElementById('estado').value = retorno.uf;
 }
 
+//validando o campo cep.
+const eNumero = (numero) =>  /^[0-9]+$/.test(numero);
+const validaCep = (cep) => cep.length == 8 && eNumero(cep);
+
 const pesquisaCep = async() =>{
     const cep = document.getElementById('cep').value;
 
     const url =  `https://viacep.com.br/ws/${cep}/json/`;
     // fetch(url).then(response => response.json()).then(console.log);
+   if(validaCep(cep)){
+    
+    const busca = await fetch(url);
+    const retorno = await busca.json();
+    if(retorno.hasOwnProperty('erro')){
+        document.getElementById('endereco').value = 'Cep não encontrado!!';
+ 
+    }else{
+     preencherForm(retorno);
+    }
 
-   const busca = await fetch(url);
-   const retorno = await busca.json();
-    preencherForm(retorno);
+   }else{
+    document.getElementById('endereco').value = 'Cep incorreto, Digitar somente número!!';
+   }
+  
+    
+   
 }
+
 
 
 document.getElementById('cep').addEventListener('focusout', pesquisaCep);
